4c6d4d7502bbe05e3bf8ceaceac8b25a970f18e4
soramach <soram4chi@gmail.com>
2025-10-18 21:56:58 +0900

    🎨 TaskContollerの追加

--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/folders/{id}/tasks", [TaskController::class, "index"])->name("tasks.index");
