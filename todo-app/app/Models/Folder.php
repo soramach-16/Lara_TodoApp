e554d89611499d3ad24e2523c0e62994205efbc3
soramach <soram4chi@gmail.com>
2025-10-18 23:00:45 +0900

    ⚡️ Taskテーブルをリレーションで取得するように修正

--- todo-app/app/Models/Folder.php
+++ todo-app/app/Models/Folder.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    //
+    
+    /*
+    * フォルダクラスとタスククラスを関連付けするメソッド
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    */
+    public function tasks()
+    {
+        return $this->hasMany('App\Models\Task');
+    }
 }
