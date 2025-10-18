b3b46270428a310aaf18102bb3115a47a71047ce
soramach <soram4chi@gmail.com>
2025-10-18 22:29:24 +0900

    ✨ フォルダー一覧表示機能追加

--- todo-app/app/Http/Controllers/TaskController.php
+++ todo-app/app/Http/Controllers/TaskController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    public function index(){
-        return 'hello controller';
+    public function index(int $id){
+        $folders = Folder::all();
+
+        return view("tasks/index",[
+            'folders' => $folders,
+            'folder_id' => $id
+        ]);
     }
 }
