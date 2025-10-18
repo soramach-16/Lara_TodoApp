b9f0806fa96e35ac2dc969d72b3a831a71c37dad
soramach <soram4chi@gmail.com>
2025-10-18 22:53:07 +0900

    🎨 タスク表示機能追加

--- todo-app/app/Http/Controllers/TaskController.php
+++ todo-app/app/Http/Controllers/TaskController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
-use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     public function index(int $id){
         $folders = Folder::all();
 
+        $folder = Folder::find($id);
+
+        $tasks = Task::where('folder_id', $folder->id)->get();
+
         return view("tasks/index",[
             'folders' => $folders,
-            'folder_id' => $id
+            'folder_id' => $folder->id,
+            'tasks' => $tasks
         ]);
     }
 }
