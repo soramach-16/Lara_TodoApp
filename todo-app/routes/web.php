187468a16d6c5b4801ffcb0035d96caa75ec8e3a
soramach <soram4chi@gmail.com>
2025-10-19 14:35:37 +0900

    🎨 タスク削除機能追加

--- todo-app/routes/web.php
+++ todo-app/routes/web.php
@@ -11,3 +11,5 @@ Route::get('/folders/{id}/tasks/create', [TaskController::class,"showCreateForm"
 Route::post('/folders/{id}/tasks/create', [TaskController::class,"create"]);
 Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,"showEditForm"])->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class,"edit"]);
+Route::get('/folders/{id}/tasks/{task_id}/delete', [TaskController::class,"showDeleteForm"])->name('tasks.delete');
+Route::post('/folders/{id}/tasks/{task_id}/delete', [TaskController::class,"delete"]);
